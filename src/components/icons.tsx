f1c1893c8586cf21ce79435fcd6f84e4fcc7a2f9
Russ ArtH <loanbox55@gmail.com>
2025-07-06 09:09:43 +0000

    looks great now can we just mak sure the 'M" is centered

--- src/components/icons.tsx
+++ src/components/icons.tsx
@@ -5,7 +5,7 @@ export function Logo(props: SVGProps<SVGSVGElement>) {
     <svg viewBox="0 0 40 40" xmlns="http://www.w3.org/2000/svg" {...props}>
       <rect width="40" height="40" rx="8" fill="currentColor" />
       <path
-        d="M13 28V12h6l5 10 5-10h6v16h-4.5V17.5L25 27l-5.5-9.5V28H13z"
+        d="M9 28V12h6l5 10 5-10h6v16h-4.5V17.5L21 27l-5.5-9.5V28H9z"
         fill="white"
       />
     </svg>
