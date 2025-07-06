13c494b3b0d51ee83b0a1fd4e2a3a17ab2b41a12
Russ ArtH <loanbox55@gmail.com>
2025-07-06 08:53:52 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/hooks/use-mobile.tsx
+++ src/hooks/use-mobile.tsx
@@ -3,9 +3,12 @@ import * as React from "react"
 const MOBILE_BREAKPOINT = 768
 
 export function useIsMobile() {
+  const [isMounted, setIsMounted] = React.useState(false)
   const [isMobile, setIsMobile] = React.useState(false)
 
   React.useEffect(() => {
+    setIsMounted(true)
+
     const checkIsMobile = () => {
       setIsMobile(window.innerWidth < MOBILE_BREAKPOINT)
     }
@@ -17,6 +20,10 @@ export function useIsMobile() {
       window.removeEventListener("resize", checkIsMobile)
     }
   }, [])
+
+  if (!isMounted) {
+    return false
+  }
 
   return isMobile
 }
