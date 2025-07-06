9470d9705d1da51c7bbcb68478c4d7ffcdef2bd2
Russ ArtH <loanbox55@gmail.com>
2025-07-06 21:53:30 +0000

    The NextJS app server is having trouble starting. Please identify what c

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,9 @@
+
 import type {NextConfig} from 'next';
 
 const nextConfig: NextConfig = {
   /* config options here */
-  allowedDevOrigins: ['*.cloudworkstations.dev'],
+  allowedDevOrigins: ['https://*.cloudworkstations.dev'],
   typescript: {
     ignoreBuildErrors: true,
   },
