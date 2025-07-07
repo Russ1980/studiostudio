55d22ad8c7eea977586a16bb1a0ef47a6d86235f
Russ ArtH <loanbox55@gmail.com>
2025-07-07 04:24:55 +0000

    improve the look of the contact page see picture

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -3,7 +3,7 @@ import { Button } from "@/components/ui/button";
 import { Label } from "@/components/ui/label";
 import { Input } from "@/components/ui/input";
 import { Textarea } from "@/components/ui/textarea";
-import { Phone, Mail, MapPin } from "lucide-react";
+import { Mail, MapPin } from "lucide-react";
 
 
 export default function ContactPage() {
