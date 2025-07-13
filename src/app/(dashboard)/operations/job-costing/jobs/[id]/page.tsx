103bbfd05505d83b93e778027a59ca08684e2074
Russ ArtH <loanbox55@gmail.com>
2025-07-13 22:35:11 +0000

    fix these issues from the latest deployment and all repeat errorsa; This

--- src/app/(dashboard)/operations/job-costing/jobs/[id]/page.tsx
+++ src/app/(dashboard)/operations/job-costing/jobs/[id]/page.tsx
@@ -3,8 +3,8 @@ import { getJobDetails } from "@/lib/actions";
 import { JobDetailsClientPage } from "@/app/(dashboard)/operations/job-costing/jobs/[id]/job-details-client";
 import { notFound } from "next/navigation";
 
-export default async function JobDetailsPage({ params }: { params: Promise<{ id: string }> }) {
-  const { id } = await params;
+export default async function JobDetailsPage({ params }: { params: { id: string } }) {
+  const { id } = params;
   const job = await getJobDetails(id);
 
   if (!job) {
